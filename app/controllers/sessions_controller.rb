f260028b21f57912ece58997b91c81f8ecb79600
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-05 14:13:36 -0400

    commit tree e18a74896ce9835250867db0e827ca1492cfc22c

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  
+  
+  def create
+    # @user = User.find_by(name: params[:user][:name])
+    # if @user && @user.authenticate(params[:user][:password])
+    #     session[:user_id] = @user.id
+    #     redirect_to '/user'
+    # else 
+    #     render :new
+    # end
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+  end
+
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
